bc78b834bc9f16101d444f13b459375ea480f6ae
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-05-24 15:20:32 +0300

    Update ServicesClient.tsx

--- app/services/ServicesClient.tsx
+++ app/services/ServicesClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState, useEffect, useCallback } from "react";
+import { useRef, useState, useEffect, } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
