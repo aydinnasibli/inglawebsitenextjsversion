317dc95efcd95030a0aab67c318593b69dd20c01
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2026-01-22 22:11:16 +0400

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
 // app/layout.tsx
-import React from 'react';
-import { Inter } from 'next/font/google';
-import type { Metadata } from 'next';
-import './globals.css';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-
-const inter = Inter({ subsets: ['latin'] });
+import React from "react";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Ingla School | Azərbaycan',
-  description: 'Ingla School - Təhsil mərkəzi, Preschool, Xaricdə təhsil',
+  title: "Ingla School | Azərbaycan",
+  description: "Ingla School - Təhsil mərkəzi, Preschool, Xaricdə təhsil",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="az" >
-      <body className={`${inter.className} min-h-screen bg-black text-gray-300 `}>
+    <html lang="az">
+      <body
+        className={`${inter.className} min-h-screen bg-black text-gray-300 `}
+      >
+        <GoogleAnalytics gaId="G-4680L6LTVN" />
         <Navbar />
-        <main className="flex-grow">
-          {children}
-        </main>
+        <main className="grow">{children}</main>
         <Footer />
       </body>
     </html>
